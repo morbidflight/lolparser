a1b3f86a8f2ed7f7e4512a3ff7fa6720b36e8831
morbidflight <morbidflight@gmail.com>
2014-12-18 10:36:44 -0600

    It grabs the username of the person and ignores parens now, whee.

--- lolparser.py
+++ lolparser.py
@@ -7,9 +7,10 @@ from collections import Counter
 champstats = Counter()
 filepath = "/Volumes/Macintosh HD/Applications/League of Legends.app/C\
 ontents/LoL/Logs/Game - R3d Logs/"
-gamecount = 0
-speccount = 0
-borkcount = 0
+gamecount = 0  # counts the number of games played by the person
+speccount = 0  # counts the number of games spectated by the person
+borkcount = 0  # counts the number of times the game was abandoned
+summoner = ''  # sets the summoner name of the person
 
 # iterates over each file in the log directory and parses the champ stats
 for f in os.listdir(filepath):
@@ -26,9 +27,10 @@ for f in os.listdir(filepath):
           played = re.search(r'netUID: (\d)', str)
           if played:
                UID = played.group(1)
-               name = re.search(r'Spawning champion (.*?) .+ clientID {}'.format(UID), str)
+               name = re.search(r'Spawning champion \((.*?)\) .+ clientID {} and summonername \((.+?)\)'.format(UID), str)
                gamecount += 1
                champstats[name.group(1)] += 1
+               summoner = name.group(2)
           else:
                speccount += 1
      else:
@@ -37,7 +39,8 @@ for f in os.listdir(filepath):
                borkcount += 1
      temp.close()
 
+print 'Analyzing logs for {}.'.format(summoner)
 print 'You played {} games.'.format(gamecount)
 print 'You spectated {} games.'.format(speccount)
 print 'You broke {} games. Rito pls!'.format(borkcount)
-print champstats.most_common()
+print champstats.most_common(3)
